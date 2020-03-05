9fe8c72e984a8abfe8a7024bf6dfaac6cd7f229e
suminies <suminies@gmail.com>
2020-03-06 07:53:58 +0900

    Color Add 컬러 팔레트 크기 균일하지 않음 #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:cosmetic_app/color_select/color_select.dart';
+import 'package:cosmetic_app/color_add/color_add.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return MaterialApp(
-      home: ColorSelect(),
+      home: ColorAdd(),
     );
   }
 }
