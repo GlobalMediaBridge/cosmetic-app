cba17d3f248306a26d780a206e95965a8cc2c892
suminies <suminies@gmail.com>
2020-03-06 04:23:57 +0900

    Tutorial1 2 3 상단 노란색 네모부분 텍스트가 안나옴. 스타일 수정 필요 #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:cosmetic_app/splash/splash.dart';
+import 'package:cosmetic_app/tutorial3/tutorial3.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return MaterialApp(
-      home: Splash(),
+      home: Tutorial3(),
     );
   }
 }
