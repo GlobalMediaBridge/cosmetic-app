74290d2f3fff99c55c67127ef77401ea80d1962c
suminies <suminies@gmail.com>
2020-03-06 04:36:06 +0900

    Start done #6

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 
-import 'package:cosmetic_app/tutorial3/tutorial3.dart';
+import 'package:cosmetic_app/start/start.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(App());
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
   
     return MaterialApp(
-      home: Tutorial3(),
+      home: Start(),
     );
   }
 }
