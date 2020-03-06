c3531f47785c283936884d944dbc546a1333e3de
ParkEunwoo <dmsdn960@gmail.com>
2020-03-06 16:40:25 +0900

    색상 추출 함수 구현 #10

--- lib/store/server.dart
+++ lib/store/server.dart
@@ -1,26 +1,45 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class Server {
-  static final String url="http://localhost:5000";
+  static final String url = "http://localhost:5000";
   static final Dio dio = Dio();
 
   static Future<String> uploadPalette(File image) async {
     FormData formData = FormData.fromMap({
-      "image": await MultipartFile.fromFile(image.path,
-          filename: 'palette.jpg'),
+      "image":
+          await MultipartFile.fromFile(image.path, filename: 'palette.jpg'),
     });
     Response response = await dio.post("$url/palette", data: formData);
     return response.data.toString();
   }
 
-  static Future<String> uploadFace(File image) async {
+  static Future<String> uploadFace(String id, File image) async {
     FormData formData = FormData.fromMap({
-      "image": await MultipartFile.fromFile(image.path,
-          filename: 'face.jpg'),
+      "image": await MultipartFile.fromFile(image.path, filename: 'face.jpg'),
     });
-    Response response = await dio.post("$url/upload", data: formData);
+    Response response = await dio.post(
+      "$url/upload",
+      data: formData,
+      options: Options(
+        headers: {"id": id},
+      ),
+    );
     return response.data.toString();
+  }
+
+  static Future<Color> extractColor(String id, int x, int y) async {
+    Response response = await dio.post(
+      "$url/extract",
+      data: {"x": x, "y": y},
+      options: Options(
+        headers: {"id": id},
+      ),
+    );
+    List<double> data = response.data;
+    Color color = Color.fromARGB(data[3].toInt(), data[2].toInt(), data[1].toInt(), data[0].toInt());
+    return color;
   }
 }
