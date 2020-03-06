7f142561aafb3a1ba63e8f59377cefda6fd17306
ParkEunwoo <dmsdn960@gmail.com>
2020-03-07 00:53:14 +0900

    start 화면 자잘한 것들 수정 #1

--- lib/screens/start/start.dart
+++ lib/screens/start/start.dart
@@ -64,11 +64,9 @@ class _StartState extends State<Start> with TickerProviderStateMixin {
   
     return Scaffold(
       body: Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 247, 7, 70),
-        ),
+        color: Color.fromARGB(255, 247, 7, 70),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
               margin: EdgeInsets.only(top: 230),
@@ -87,8 +85,6 @@ class _StartState extends State<Start> with TickerProviderStateMixin {
               ),
             ),
             Container(
-              width: 367,
-              height: 166,
               margin: EdgeInsets.only(top: 28),
               child: StartWidgetAnimation1Element2(
                 animationController: this.buttongroupAnimationController,
@@ -131,7 +127,7 @@ class _StartState extends State<Start> with TickerProviderStateMixin {
                         padding: EdgeInsets.all(0),
                         child: Text(
                           "사진앨범",
-                          textAlign: TextAlign.left,
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                             color: AppColors.primaryText,
                             fontFamily: "NanumBarunGothic",
@@ -160,7 +156,7 @@ class _StartState extends State<Start> with TickerProviderStateMixin {
                         padding: EdgeInsets.all(0),
                         child: Text(
                           "사용설명",
-                          textAlign: TextAlign.left,
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                             color: Color.fromARGB(255, 255, 255, 255),
                             fontFamily: "NanumBarunGothic",
@@ -179,12 +175,11 @@ class _StartState extends State<Start> with TickerProviderStateMixin {
               margin: EdgeInsets.only(bottom: 20),
               child: Text(
                 "© 2020 app name",
-                textAlign: TextAlign.left,
+                textAlign: TextAlign.center,
                 style: TextStyle(
                   color: Color.fromARGB(255, 0, 0, 0),
                   fontWeight: FontWeight.w400,
                   fontSize: 14,
-                  height: 1.14286,
                 ),
               ),
             ),
