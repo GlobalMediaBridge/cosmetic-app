68db87210eec1d45c0de50e61bf9301e573e8340
ParkEunwoo <dmsdn960@gmail.com>
2020-03-06 22:18:41 +0900

    카메라 화면 코드 리펙토링 #9

--- lib/screens/camera_color.dart
+++ lib/screens/camera_color.dart
@@ -51,241 +51,69 @@ class _CameraColorState extends State<CameraColor> {
           future: _initializeControllerFuture,
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return Stack(
-                alignment: Alignment.bottomCenter,
+              return Column(
                 children: <Widget>[
-                  CameraPreview(_controller),
-                  Column(
+                  Stack(
+                    alignment: Alignment.center,
                     children: <Widget>[
+                      AspectRatio(
+                          aspectRatio: 3.0 / 4.0,
+                          child: CameraPreview(_controller)),
                       Positioned(
                         top: 24,
                         child: Container(
+                          alignment: Alignment.center,
                           width: 330,
                           height: 28,
                           decoration: BoxDecoration(
                             color: Color.fromARGB(128, 255, 255, 255),
                             borderRadius: BorderRadius.all(Radius.circular(14)),
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "손목 발색 사진을 촬영해주세요.",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 247, 7, 70),
-                                  fontFamily: "NanumBarunGothic",
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
+                          child: Text(
+                            "손목 발색 사진을 촬영해주세요.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 247, 7, 70),
+                              fontFamily: "NanumBarunGothic",
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                            ),
                           ),
                         ),
                       ),
                     ],
                   ),
+                  Expanded(
+                      child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          width: 46,
+                          height: 46,
+                          decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(7))),
+                          child: FlatButton(
+                            onPressed: () => this.onViewPressed(context),
+                          ),
+                        ),
+                        FlatButton(
+                          onPressed: () => this.onButtonTwoPressed(context),
+                          child: Image.asset(
+                            "assets/images/camera_main.png",
+                          ),
+                        ),
+                        FlatButton(
+                            onPressed: () => this.onButtonPressed(context),
+                            child: Image.asset(
+                              "assets/images/lotation.png",
+                            )),
+                      ],
+                    ),
+                  ))
                 ],
-              );
-              Container(
-                constraints: BoxConstraints.expand(),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Positioned(
-                      left: 0,
-                      top: 0,
-                      right: 0,
-                      bottom: 0,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: Container(
-                              height: 500,
-                              child: Stack(
-                                alignment: Alignment.center,
-                                children: [
-                                  Positioned(
-                                    left: 0,
-                                    top: 0,
-                                    right: 0,
-                                    child: CameraPreview(_controller),
-                                  ),
-                                  Positioned(
-                                    top: 24,
-                                    child: Container(
-                                      width: 330,
-                                      height: 28,
-                                      decoration: BoxDecoration(
-                                        color:
-                                            Color.fromARGB(128, 255, 255, 255),
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(14)),
-                                      ),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text(
-                                            "손목 발색 사진을 촬영해주세요.",
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 247, 7, 70),
-                                              fontFamily: "NanumBarunGothic",
-                                              fontWeight: FontWeight.w700,
-                                              fontSize: 14,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Spacer(),
-                          Container(
-                            height: 167,
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Positioned(
-                                  left: 0,
-                                  right: 0,
-                                  bottom: 0,
-                                  child: Container(
-                                    height: 167,
-                                    decoration: BoxDecoration(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                    ),
-                                    child: Container(),
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 30,
-                                  bottom: 59,
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.stretch,
-                                    children: [
-                                      Align(
-                                        alignment: Alignment.bottomLeft,
-                                        child: Container(
-                                          width: 46,
-                                          height: 46,
-                                          margin: EdgeInsets.only(bottom: 2),
-                                          child: FlatButton(
-                                            onPressed: () =>
-                                                this.onViewPressed(context),
-                                            color: Color.fromARGB(255, 0, 0, 0),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(7)),
-                                            ),
-                                            textColor:
-                                                Color.fromARGB(255, 0, 0, 0),
-                                            padding: EdgeInsets.all(0),
-                                            child: Text(
-                                              "",
-                                              textAlign: TextAlign.left,
-                                              style: TextStyle(
-                                                color: Color.fromARGB(
-                                                    255, 0, 0, 0),
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 12,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Container(
-                                        width: 48,
-                                        height: 48,
-                                        child: FlatButton(
-                                          onPressed: () =>
-                                              this.onButtonPressed(context),
-                                          color: Color.fromARGB(0, 0, 0, 0),
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(0)),
-                                          ),
-                                          textColor: Color.fromARGB(
-                                              255, 255, 255, 255),
-                                          padding: EdgeInsets.only(top: 24),
-                                          child: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: [
-                                              Image.asset(
-                                                "assets/images/lotation.png",
-                                              ),
-                                              SizedBox(
-                                                height: 10,
-                                              ),
-                                              Text(
-                                                "Click me",
-                                                textAlign: TextAlign.left,
-                                                style: TextStyle(
-                                                  color: Color.fromARGB(
-                                                      255, 255, 255, 255),
-                                                  fontWeight: FontWeight.w400,
-                                                  fontSize: 12,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 51,
-                      child: FlatButton(
-                        onPressed: () => this.onButtonTwoPressed(context),
-                        color: Color.fromARGB(0, 54, 54, 54),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(33)),
-                        ),
-                        textColor: Color.fromARGB(255, 0, 0, 0),
-                        padding: EdgeInsets.all(0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              "assets/images/camera_main.png",
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "",
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 0, 0, 0),
-                                fontWeight: FontWeight.w400,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               );
             } else {
               return Center(child: CircularProgressIndicator());
